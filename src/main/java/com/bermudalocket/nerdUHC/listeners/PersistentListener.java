8d950a16b7de871d941981ae0f0e5eaa35ca3dd1
bermudalocket <bermudalckt@gmail.com>
2018-01-27 15:35:56 -0500

    Scoreboard testing, updated pom.xml

--- src/main/java/com/bermudalocket/nerdUHC/listeners/PersistentListener.java
+++ src/main/java/com/bermudalocket/nerdUHC/listeners/PersistentListener.java
@@ -1,7 +1,5 @@
 package com.bermudalocket.nerdUHC.listeners;
 
-import java.util.UUID;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
@@ -11,6 +9,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
 import com.bermudalocket.nerdUHC.NerdUHC;
@@ -22,41 +21,39 @@ import com.bermudalocket.nerdUHC.modules.UHCMatchState;
 public class PersistentListener implements Listener {
 
 	private NerdUHC plugin;
-	
+
 	public PersistentListener(NerdUHC plugin) {
 		this.plugin = plugin;
 	}
-	
+
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent e) {
-		
-		UUID player = e.getPlayer().getUniqueId();
-		Player p = Bukkit.getPlayer(player);
+
+		Player p = e.getPlayer();
+		if (p == null)
+			return;
+
 		UHCMatch match = plugin.matchHandler.getMatchByPlayer(p);
-		
+		Scoreboard board;
+
 		if (match == null) {
 			UHCMatch currentmatch = plugin.matchHandler.getMatch();
 			UHCGameMode mode = currentmatch.getGameMode();
-			
+
 			p.teleport(currentmatch.getSpawn());
 			p.setGameMode(GameMode.SURVIVAL);
-			
-			BukkitRunnable setScoreboardTask = new BukkitRunnable() {
-				@Override
-				public void run() {
-					p.setScoreboard(currentmatch.getScoreboard());
-				}
-			};
-			setScoreboardTask.runTaskLater(plugin, 1);
-			
-			plugin.getLogger().info(p.getName() + " logged in. Assigning scoreboard " + currentmatch.getScoreboard().toString());
+
+			board = currentmatch.getScoreboard();
+
+			plugin.getLogger()
+					.info(p.getName() + " logged in. Assigning scoreboard " + currentmatch.getScoreboard().toString());
 			plugin.scoreboardHandler.showTeamCountCapacity(currentmatch);
 
 			UHCLibrary.LIB_WELCOME.rep(p, "%t", mode.toString());
-			
+
 			if (currentmatch.getMatchState() == UHCMatchState.PREGAME) {
 				currentmatch.addPlayer(p);
-				
+
 				if (mode == UHCGameMode.SOLO) {
 					UHCLibrary.LIB_SOLO_JOIN.get(p);
 					UHCLibrary.LIB_SPEC.get(p);
@@ -71,16 +68,24 @@ public class PersistentListener implements Listener {
 				UHCLibrary.LIB_IN_PROGRESS.get(p);
 			}
 		} else {
-			p.setScoreboard(match.getScoreboard());
+			board = match.getScoreboard();
 			plugin.scoreboardHandler.showTeamCountCapacity(match);
 		}
+		
+		BukkitRunnable setScoreboardTask = new BukkitRunnable() {
+			@Override
+			public void run() {
+				p.setScoreboard(board);
+			}
+		};
+		setScoreboardTask.runTaskLater(plugin, 1);
 	}
-	
+
 	@EventHandler
 	public void onPlayerChat(AsyncPlayerChatEvent e) {
 		Player p = e.getPlayer();
 		UHCMatch match = plugin.matchHandler.getMatchByPlayer(p);
-		
+
 		ChatColor color;
 		if (match == null) {
 			plugin.getLogger().info("match null");
@@ -94,12 +99,12 @@ public class PersistentListener implements Listener {
 				color = t.getColor();
 			}
 		}
-		
+
 		for (Player player : Bukkit.getOnlinePlayers()) {
 			player.sendMessage("<" + color + p.getName() + ChatColor.WHITE + "> " + e.getMessage());
 		}
-		
+
 		e.setCancelled(true);
 	}
-	
+
 }
